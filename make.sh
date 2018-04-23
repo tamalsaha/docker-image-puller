cef7722f1f708ff57e378a358f38d406f421b396
tamal <tamal@appscode.com>
2018-04-23 16:27:17 -0700

    added-all

--- make.sh
+++ make.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -xeou pipefail
 
-go build main.go
+go build -v -o docker-image-puller main.go
+chmod +x docker-image-puller
 
 docker build -t appscode/docker-image-puller .
 docker push appscode/docker-image-puller
 
+rm -rf docker-image-puller
