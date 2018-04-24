b0a3860f025b2df1c336e7381d1d184d620d8eec
tamal <tamal@appscode.com>
2018-04-23 17:56:03 -0700

    added-all

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ import (
 	_ "k8s.io/kubernetes/pkg/credentialprovider/aws"
 	_ "k8s.io/kubernetes/pkg/credentialprovider/azure"
 	_ "k8s.io/kubernetes/pkg/credentialprovider/gcp"
+	// _ "k8s.io/kubernetes/pkg/credentialprovider/rancher"
 	"k8s.io/kubernetes/pkg/util/parsers"
 )
 
